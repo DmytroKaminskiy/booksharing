1adf70ce05a49d07ae90ea18ce820eb52c92c4d1
dmytro.kaminskyi92@gmail.com <dmytro.kaminskyi92@gmail.com>
2021-01-21 21:16:23 +0200

    add book model

--- app/books/views.py
+++ app/books/views.py
@@ -1,24 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-import string
-import random
+from books.models import Book
 
 
-# /hello-world/
-# def hello_world():
-#     return "Hello World"
-# from flask import request
+def book_list(request):
 
+    response_content = ''
 
-def generate_password(length: int = 10) -> str:
-    password = ''
-    for _ in range(length):
-        password += random.choice(string.ascii_letters)
-    return password
+    for book in Book.objects.all():  # Book.objects.all() - SELECT * FROM books_book;
+        response_content += f'ID: {book.id}, Author: {book.author} <br/>'
 
-
-def hello_world(request):  # /hello-world/
-    length = int(request.GET.get('length') or 10)
-    password = generate_password(length)
-    return HttpResponse(password)
+    return HttpResponse(response_content)
